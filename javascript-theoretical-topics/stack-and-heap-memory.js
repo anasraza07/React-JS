7d66fd4d4ca1112f5a5435fd8837ec4f04312934
anasraza07 <ar470684@gmail.com>
2024-07-13 00:49:05 +0500

    stack and heap memory lecture javascript understood and noted

---
+++ javascript-theoretical-topics/stack-and-heap-memory.js
@@ -0,0 +1,18 @@
+// Primitive D  atatype
+let myYoutubeName = 'hiteshchoudhrydotcom'
+let anotherName = myYoutubeName;
+anotherName = 'chaiaurcode'
+console.log(anotherName)
+console.log(myYoutubeName)
+
+let userOne = {
+    email: 'user@google.com',
+    upi: 'user@ybl'
+}
+
+// Non-primitive Datatype
+let userTwo = userOne;
+userTwo.email = 'hitesh@google.com'
+
+console.log(userTwo.email);
+console.log(userOne.email);
