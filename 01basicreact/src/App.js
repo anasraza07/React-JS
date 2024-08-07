7437a5f25ebf4a67317e17f1eda81aa279937dd5
anasraza07 <ar470684@gmail.com>
2024-08-08 00:24:10 +0500

    lesson 03 completed

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,12 @@
+import Greet from "./Greet";
+
+function App() {
+  return (
+    <>
+      <Greet />
+      <h1>Hey</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import Greet
+  from "./Greet"
+function App() {
+  return (
+    <>
+      <h1>Hello world! react with vite</h1>
+      <Greet />
+    </>
+  )
+}
+
+export default App
